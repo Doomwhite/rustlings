4ed08461a7a56d0c172587760c436e3e44931a23
DooMWhite <40442835+Doomwhite@users.noreply.github.com>
2023-05-31 22:37:02 -0300

    Merges stuff (#2)
    * Finishes the exercises until hashmaps1
    
    * Finishes the exercises until hashmaps2
    
    * Finishes the exercises until quiz2

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -18,8 +18,6 @@
 // - The output element is going to be a Vector of strings.
 // No hints this time!
 
-// I AM NOT DONE
-
 pub enum Command {
     Uppercase,
     Trim,
@@ -27,13 +25,26 @@ pub enum Command {
 }
 
 mod my_module {
+    use std::usize;
+
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
+            match command {
+                Command::Trim => output.push(string.trim().into()),
+                Command::Uppercase => output.push(string.to_uppercase().into()),
+                Command::Append(n) => {
+                    let mut a = string.clone();
+                    for _i in 0..*n {
+                        a.push_str("bar");
+                    }
+                    output.push(a.into());
+                }
+            };
             // TODO: Complete the function body. You can do it!
         }
         output
@@ -43,8 +54,8 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
     use super::Command;
+    use crate::my_module::*;
 
     #[test]
     fn it_works() {
